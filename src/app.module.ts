6e5c1a1357274e184c414706158a17bced8fb54b
new-username <rahulknit007@gmail.com>
2025-10-15 13:08:53 +0530

    remove some fiels

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,8 +9,6 @@ import { ErrorLoggerService } from './error-logger/error-logger.service';
 import { AesService } from './services/aes/aes.service';
 import { AuthService } from './auth/auth.service';
 import { JwtService } from '@nestjs/jwt';
-import { UserService } from './user/user.service';
-import { UserController } from './user/user.controller';
 import { AuthController } from './auth/auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { ClusterService } from "./services/cluster/cluster.service";
@@ -30,8 +28,8 @@ import { AccountService} from './accounts/account.service';
         signOptions: { expiresIn: '15m' },
       }),
     }),],
-  controllers: [AppController,UserController,AuthController,LeadController,AccountController],
-  providers: [AppService,UtilService,DbService,ErrorLoggerService,AesService,AuthService,UserService,
+  controllers: [AppController,AuthController,LeadController,AccountController],
+  providers: [AppService,UtilService,DbService,ErrorLoggerService,AesService,AuthService,
     AuthService,ClusterService,LeadService,AccountService],
 })
 //with middle ware 
